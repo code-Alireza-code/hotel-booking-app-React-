defe6f1473f90c5129a175f8a677112f00e97e7a
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-02-01 11:40:59 +0330

    start project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center mt-4 text-2xl font-semibold">
+      hotel booking App (TS + TailwindCSS)
+    </div>
+  );
+}
+
+export default App;
