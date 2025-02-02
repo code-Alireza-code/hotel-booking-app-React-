9912b3ed16b43afcece5b0c4c829b1fda8d46cac
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-02-02 22:28:43 +0330

    add AppLayout and add different routes

---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+function AppLayout() {
+  return (
+    <div className="mt-4 flex justify-between items-stretch h-[calc(100vh - 130px)]">
+      <div className="w-[30%] overflow-y-scroll pr-4 bg-red-200">
+        <Outlet />
+      </div>
+      <div className="grow bg-secondary-100 relative">map component</div>
+    </div>
+  );
+}
+
+export default AppLayout;
