8a6928489122b38dabed97a8d6c9dd92585e19b3
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-02-02 11:53:21 +0330

    add react-hot-toast and axios packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="container max-w-7xl ">
+      <Toaster />
       <Header />
     </div>
   );
