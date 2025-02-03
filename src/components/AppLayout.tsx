ddfc21db15c23a29bcdba6340029f75c474e31cc
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-02-03 14:37:26 +0330

    fix Applayout height style

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 function AppLayout() {
   return (
-    <div className="mt-4 flex justify-between items-stretch h-[calc(100vh - 130px)]">
-      <div className="w-[30%] overflow-y-scroll pr-4 bg-red-200">
+    <div className="mt-4 flex justify-between items-stretch h-[calc(100vh-130px)]">
+      <div className="w-[30%] overflow-y-scroll pr-4 ">
         <Outlet />
       </div>
       <div className="grow bg-secondary-100 relative">map component</div>
