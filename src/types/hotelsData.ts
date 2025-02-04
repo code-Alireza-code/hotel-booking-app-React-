8780c8aa998d4912667960af7976936bb43dfb16
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-02-04 13:04:07 +0330

    update HotelDataType with new types

--- src/types/hotelsData.ts
+++ src/types/hotelsData.ts
@@ -1,10 +1,11 @@
 export type HotelDataType = {
   id: string;
-  latitude: string;
-  longitude: string;
+  latitude: number;
+  longitude: number;
   thumbnail_url: string;
   name: string;
   smart_location: string;
   price: number;
   xl_picture_url: string;
+  host_location: string;
 };
