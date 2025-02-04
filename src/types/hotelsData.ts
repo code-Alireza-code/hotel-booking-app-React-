444d135e8953bbbbce046b080a4b6841c53c276e
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-02-04 09:50:40 +0330

    update unused rule and add global hotelDataType

---
+++ src/types/hotelsData.ts
@@ -0,0 +1,10 @@
+export type HotelDataType = {
+  id: string;
+  latitude: string;
+  longitude: string;
+  thumbnail_url: string;
+  name: string;
+  smart_location: string;
+  price: number;
+  xl_picture_url: string;
+};
